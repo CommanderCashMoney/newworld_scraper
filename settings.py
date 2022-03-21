47668581d04c0e9686494364ebda16957f4c6235
Jack Ward <jackward84@gmail.com>
2022-03-21 21:12:44 +1300

    add settings module

---
+++ settings.py
@@ -0,0 +1,40 @@
+from enum import Enum
+from pathlib import Path
+
+from pydantic import BaseSettings
+
+class Environment(Enum):
+    prod = "prod"
+    dev = "dev"
+
+
+class Settings(BaseSettings):
+    environment: Environment = Environment.dev
+    nwmp_dev_api_host: str = "http://localhost:8080/"
+    nwmp_prod_api_host: str = "https://nwmarketprices.com/"
+    # user/pass can be set for development purposes in .env to avoid having to type each time
+    api_username: str = ""
+    api_password: str = ""
+    APPDATA: str = None
+
+    def app_data_folder(self, relative_path: str) -> Path:
+        if self.APPDATA is None:
+            raise ValueError("No appdata folder found. TODO: set a default")
+        app_data_base = Path(self.APPDATA)
+        app_data_folder = app_data_base / "Cash Money Development" / "Trading Post Scraper" / relative_path
+        app_data_folder.mkdir(parents=True, exist_ok=True)
+        return app_data_folder
+
+    @property
+    def temp_app_data(self) -> Path:
+        return self.app_data_folder("temp")
+
+    @property
+    def is_dev(self) -> bool:
+        return self.environment == Environment.dev
+
+    class Config:
+        env_file = ".env"
+
+
+SETTINGS = Settings()
