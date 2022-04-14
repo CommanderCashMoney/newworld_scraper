afb2fb247e4fa8b75a90c64b05f631f0ab9b56c7
Jack Ward <jackward84@gmail.com>
2022-04-14 15:34:18 +1200

    bump version

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ class KeyBindings(BaseSettings):
 
 
 class Settings(BaseSettings):
-    VERSION = "1.2.1"
+    VERSION = "1.2.2"
 
     environment: Environment = Environment.prod
     use_dev_colors: bool = False
