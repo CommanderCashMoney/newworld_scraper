694589d448290c26b5c1ff48fee3fbaed687283f
Jason <valesyde@gmail.com>
2022-11-29 12:12:27 -0800

    sectional scans

--- app/overlay/layouts/scan_options_layout.py
+++ app/overlay/layouts/scan_options_layout.py
@@ -4,7 +4,7 @@ from app.ocr.resolution_settings import res_1440p
 
 
 def sections_layout():
-    section_checkboxes = []
+    section_checkboxes = [[sg.Text('Choose the sections to scan')], [sg.HorizontalSeparator()]]
     sections = res_1440p.sections
     for s in sections:
         section_checkboxes.append([sg.Checkbox(f'{s}', key=f'{s}', default=True, enable_events=False)])
