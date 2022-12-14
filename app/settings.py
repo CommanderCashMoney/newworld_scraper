74d9ec8383ea039673a885c9e50164c873bbe6a6
Jason <valesyde@gmail.com>
2022-12-13 18:39:23 -0800

    buy order scanning

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ class KeyBindings(BaseSettings):
 
 
 class Settings(BaseSettings):
-    VERSION = "1.5.0"
+    VERSION = "1.4.6"
 
     environment: Environment = Environment.prod
     use_dev_colors: bool = False
