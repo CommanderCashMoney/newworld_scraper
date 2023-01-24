7c2b10aed3fe1c87ca3041bd314258094a991527
Jason <valesyde@gmail.com>
2023-01-24 13:15:27 -0800

    update version #

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ class KeyBindings(BaseSettings):
 
 
 class Settings(BaseSettings):
-    VERSION = "1.5.7"
+    VERSION = "1.5.8"
 
     environment: Environment = Environment.prod
     use_dev_colors: bool = False
