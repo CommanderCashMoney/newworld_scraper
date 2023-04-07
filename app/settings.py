99fb52a1ae01e38eeb1823599e76322f9657ac0b
Jason <valesyde@gmail.com>
2023-04-07 10:50:54 -0700

    lower conf score threshold because trading post ui shifted by a pixel.
    push down location of exit to desktop button because of shift in UI

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ class KeyBindings(BaseSettings):
 
 
 class Settings(BaseSettings):
-    VERSION = "1.5.9"
+    VERSION = "1.6.0"
 
     environment: Environment = Environment.prod
     use_dev_colors: bool = False
