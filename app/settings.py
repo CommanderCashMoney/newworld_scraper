6ac6a0902859a137cfba327ec04f6e6a7c2616ff
Jason <valesyde@gmail.com>
2023-07-08 11:04:16 -0700

    update references images for scroll bars for 2460x1440 and 1920x1080. These were changed with the season 2 patch.
    Create new wizard to make it easier to grab and update images

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ class KeyBindings(BaseSettings):
 
 
 class Settings(BaseSettings):
-    VERSION = "1.6.1"
+    VERSION = "1.6.2"
 
     environment: Environment = Environment.prod
     use_dev_colors: bool = False
